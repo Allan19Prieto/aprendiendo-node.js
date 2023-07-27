03002ddc2cce18363af3b77f851a7d7a1a859d13
Allan Prieto <allanbanilla@gmail.com>
2023-07-26 22:55:22 -0600

    Async await mjs

---
+++ 4.fs-async-await.mjs
@@ -0,0 +1,12 @@
+
+
+import { readFile } from 'node:fs/promises'
+
+console.log('Leyenco el primer archivo..')
+const text = await readFile('./archivo.txt', 'utf-8')
+console.log('primer texto:', text)
+console.log('Hacer cosas mientras se lee el archivo...')
+
+console.log('Leyendo el segundo archivo...')
+const secondtext = await readFile('./archivo2.txt', 'utf-8')
+console.log('Segundo texto:', secondtext)
