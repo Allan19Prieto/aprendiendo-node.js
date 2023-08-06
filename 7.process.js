1a748e45ad2ca11d74c2fa64120ca8f26b431303
Allan Prieto <allanbanilla@gmail.com>
2023-08-05 22:34:18 -0600

    Chages

---
+++ 7.process.js
@@ -0,0 +1,16 @@
+// process
+//console.log(process.argv)
+
+// Controlar el proceso y su salida
+//process.exit(0) // con 0 todo bien, con 1 es que ha habido algún error
+
+// Podemos controlar eventos del proceso
+//process.on('exit', ( ) => {
+    // Limpiar los recursos
+//})
+
+// Current working directory
+console.log(process.cwd())
+
+// platform
+console.log(process.env.PEPITO) 
