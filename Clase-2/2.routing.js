4be6659cc06ef7674ec695939e9845290d3254b0
Allan Prieto <allanbanilla@gmail.com>
2023-08-21 16:42:03 -0600

    api post

--- Clase-2/2.routing.js
+++ Clase-2/2.routing.js
@@ -2,6 +2,7 @@ const http = require('node:http')
 
 // comminJS --> module clásico de node
 const dittoJSON = require('./pokemon/ditto.json')
+const { stringify } = require('node:querystring')
 
 const processRequest = (req, res) => {
   const { method, url } = req
@@ -15,15 +16,31 @@ const processRequest = (req, res) => {
         default:
           res.statusCode = 404
           res.setHeader('Content-Type', 'text/html; charset=utf-8')
-          return res.end('<h1>404</h1>')
+          return res.end('<h1>404 Not Found</h1>')
       }
     case 'POST':
       switch (url) {
         case '/pokemon':{
-          const body = ''
+          let body = ''
+
+          // escuchar el evento data ; chunck es un buffer
+          req.on('data', chuck => {
+            body += chuck.toString()
+          })
+
+          req.on('end', () => {
+            const data = JSON.parse(body)
+            // llamar a una base de datos para guardar la info
+            res.writeHead(201, { 'Content-Type': 'application/json; charset=utf-8' })
+            data.timestamp = Date.now()
+            res.end(JSON.stringify(data))
+          })
           break
         }
-          //  ?????
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return res.end('<h1>404 Not Found</h1>')
       }
   }
 }
