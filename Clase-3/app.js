d8d8e8a287c58a158d4b6af8cbe13c80aaed8c91
Allan Prieto <allanbanilla@gmail.com>
2023-08-28 15:13:44 -0600

    Mejora con cors

--- Clase-3/app.js
+++ Clase-3/app.js
@@ -4,25 +4,39 @@
 const express = require('express') // require -> commonJS
 const movies = require('./movies.json')
 const crypto = require('node:crypto')
+const cors = require('cors')
 // const z = require('zod')
 const { validateMovie, validatePartialovie } = require('./schemas/movies')
 
 const app = express()
 app.use(express.json())
-app.disable('x-powered-by') // desabilitar el header x-powered-By: Express
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:1234',
+      'http://localhost:3000'
+    ]
 
-const ACCEPTED_ORIGINS = [
-  'http://localhost:8080',
-  'http://localhost:1234',
-  'http://localhost:3000'
-]
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('No allowed by CORS'))
+  }
+}))
+app.disable('x-powered-by') // desabilitar el header x-powered-By: Express
 
 // Todos los recursos que sean movies se identifican con /movies
 app.get('/movies', (req, res) => {
-  const origin = req.header('origin')
+  /* const origin = req.header('origin')
   if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
     res.header('Access-Control-Allow-Origin', origin)
-  }
+  } */
   const { genre } = req.query
   if (genre) {
     const filteredMovies = movies.filter(
@@ -61,10 +75,10 @@ app.post('/movies', (req, res) => {
 })
 
 app.delete('/movies/:id', (req, res) => {
-  const origin = req.header('origin')
+  /* const origin = req.header('origin')
   if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
     res.header('Access-Control-Allow-Origin', origin)
-  }
+  } */
 
   const { id } = req.params
   const movieIndex = movies.findIndex(movie => movie.id === id)
@@ -102,14 +116,14 @@ app.patch('/movies/:id', (req, res) => {
   return res.json(updateMovie)
 })
 
-app.options('/movies/:id', (req, res) => {
+/* app.options('/movies/:id', (req, res) => {
   const origin = req.header('origin')
   if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
     res.header('Access-Control-Allow-Origin', origin)
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
   }
   res.send(200)
-})
+}) */
 
 const PORT = process.env.PORT = 1234
 
