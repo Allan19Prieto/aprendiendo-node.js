1b33d521def760dfd20058f74f22f84f59a66441
Allan Prieto <allanbanilla@gmail.com>
2023-08-28 09:21:05 -0600

    changes in the api

--- Clase-3/app.js
+++ Clase-3/app.js
@@ -2,12 +2,59 @@
 // Colocamos en nom init -y
 
 const express = require('express') // require -> commonJS
+const movies = require('./movies.json')
+const crypto = require('node:crypto')
 
 const app = express()
+app.use(express.json())
 app.disable('x-powered-by') // desabilitar el header x-powered-By: Express
 
-app.get('/', (req, res) => {
-  res.json({ message: 'hola mundo' })
+// Todos los recursos que sean movies se identifican con /movies
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
+  res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => { // path-to-regexp
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+  res.status(404).json({ message: 'Movies not found ' })
+})
+
+app.post('/movies', (req, res) => {
+  const {
+    title,
+    year,
+    director,
+    duration,
+    poster,
+    genre,
+    rate
+
+  } = req.body
+
+  const newMovie = {
+    id: crypto.randomUUID(), // UUID v4  Universal unik identifai
+    title,
+    year,
+    director,
+    duration,
+    poster,
+    genre,
+    rate: rate ?? 0
+  }
+
+  // Esto no seria REST, ya que se esta guardando en memoria
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie) // actualiza la caché del cliente
 })
 
 const PORT = process.env.PORT = 1234
