7d97752fcb41640b6847ae04e2a5a88d51cc81f5
Allan Prieto <allanbanilla@gmail.com>
2023-10-02 00:15:29 -0600

    Copy Class 5

---
+++ Clase-5/app.js
@@ -0,0 +1,26 @@
+// Recordar que para inicializar un proyecto //
+// Colocamos en nom init -y
+
+import express, { json } from 'express' // require -> commonJS
+import { moviesRouter } from './routes/movies.js'
+import { corsMiddleware } from './middlewares/cors.js'
+
+// Como leer un json en ESmodules
+// import fs from 'node:fs'
+// const movies = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'))
+
+// Como leer un json en ESmodules recomendado por ahora
+
+const app = express()
+app.use(json())
+app.use(corsMiddleware())
+app.disable('x-powered-by') // desabilitar el header x-powered-By: Express
+
+// Todos los recursos que sean movies se identifican con /movies
+app.use('/movies', moviesRouter)
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+  console.log(`Servidor listening on port http://localhost:${PORT}`)
+})
